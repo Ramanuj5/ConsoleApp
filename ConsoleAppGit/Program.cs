e82c94dd3e32da7e15254cba03897ee3ace94a81
Ramanuj5 <rk.ramanujkumar@gmail.com>
2025-03-11 14:24:21 -0500

    Program got changed

--- ConsoleAppGit/Program.cs
+++ ConsoleAppGit/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World12345");
+//Console.WriteLine("Hello, World12345");
+
+
+//Write a program to print the sum of two numbers
+public class SumOfTwoNumbers
+{
+    public static void Main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        Console.WriteLine("Sum of a and b is: " + sum);
+    }
+}   
+
+
+
